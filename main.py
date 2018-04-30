dd45637fc51c10b8056f142385b70ecebd41f572
Josef Spjut <josef.spjut@gmail.com>
2018-04-30 19:15:34 -0400

    sphere with colored normal

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import matplotlib
 import matplotlib.image
 import numpy as np
+import math
 
 # include local libraries
 from src.vec3 import vec3
@@ -16,12 +17,17 @@ def hit_sphere(center, radius, r):
     b = 2.0 * oc.dot(r.direction)
     c = oc.dot(oc) - radius**2
     discriminant = b**2 - 4 * a * c
-    return discriminant > 0
+    if discriminant < 0:
+        return -1.0
+    else:
+        return (-b - math.sqrt(discriminant)) / (a * 2.0)
 
 def skybox_color(r):
     '''simple skybox function'''
-    if hit_sphere(vec3((0.0, 0.0, -1.0)), 0.5, r):
-        return vec3((1.0, 0.0, 0.0))
+    t = hit_sphere(vec3((0.0, 0.0, -1.0)), 0.5, r)
+    if t > 0.0:
+        N = (r.point_at_parameter(t) - vec3((0.0, 0.0, -1.0))).unit_vector()
+        return vec3((N.x()+1, N.y()+1, N.z()+1)) * 0.5
     unit_direction = r.direction.unit_vector()
     t = 0.5 * (unit_direction.y() + 1.0)
     return vec3((1.0, 1.0, 1.0))*(1.0-t) + vec3((0.5, 0.7, 1.0))*t
