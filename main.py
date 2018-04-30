4ce83286f7a00c48ecf5f78665a18dd7139c373a
Josef Spjut <josef.spjut@gmail.com>
2018-04-30 08:10:29 -0400

    Gradient image

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ import matplotlib
 import matplotlib.image
 import numpy as np
 
-# data = np.random.random(size=(256,256,3))
 
-data = np.zeros(shape=(256,256,3))
+if __name__ == '__main__':
+    # (y, x, colors)
+    out_shape = (100, 200, 3)
+    # data = np.random.random(size=(256,256,3))
+
+    data = np.zeros(shape=out_shape)
+
+    # fill in with gradient
+    for x in range(data.shape[0]):
+        for y in range(data.shape[1]):
+            r = x / data.shape[0]
+            g = y / data.shape[1]
+            b = 0.0
+            data[x][y] = [r, g, b]
 
 
-
-matplotlib.image.imsave('out.png', data)
+    matplotlib.image.imsave('out.png', data)
 
 
